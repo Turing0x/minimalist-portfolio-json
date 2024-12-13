5d2271a2b8254aa90b498f84770fd901ed4db573
Turing0x <rauligf97@gmail.com>
2024-12-13 17:36:26 -0500

    Refactor CV data and update configuration
    - Updated personal information in cv_english.json and cv.json, including name formatting and summary enhancements.
    - Adjusted image paths for consistency across files.
    - Improved type definitions in src/cv.d.ts for better clarity and structure.
    - Enhanced the Education and Experience sections in the respective Astro components for better readability and layout.
    - Updated pnpm-lock.yaml to version 9.0, reflecting dependency changes.
    - Minor formatting adjustments across various files for consistency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Turing0x.github.io",
-  base: "/minimalist-portfolio-json",
 });
